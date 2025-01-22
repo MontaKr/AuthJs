de478c68c869f2c3c79d8ab0124f4959164aab5b
zhoon95 <zhoon95@gmail.com>
2025-01-22 19:22:31 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import { SignOut } from "@/components/sign-out";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
+  const session = await auth(); // 현재 세션의 인증 상태 확인
+  if (!session) {
+    redirect("/sign-in"); // 세션이 존재하지 않으면 sign-in 페이지로 이동
+  }
+
   return (
     <>
       <div className="bg-gray-100 rounded-lg p-4 text-center mb-6">
