c876de375d9752ea78bd72de86c0ff9491c93db9
zhoon95 <zhoon95@gmail.com>
2025-01-31 19:21:53 +0900

    AUTH

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,2 +1,37 @@
+import Github from "next-auth/providers/github";
 import NextAuth from "next-auth";
-export const { auth, handlers } = NextAuth({ providers: [] });
+import Credentials from "next-auth/providers/credentials";
+import db from "@/lib/db";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { schema } from "./schema";
+
+const adapter = PrismaAdapter(db);
+
+export const { auth, handlers, signIn } = NextAuth({
+  adapter,
+  providers: [
+    Github,
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        const validatedCredentials = schema.parse(credentials);
+
+        const user = await db.user.findFirst({
+          where: {
+            email: validatedCredentials.email,
+            password: validatedCredentials.password,
+          },
+        });
+
+        if (!user) {
+          throw new Error("Invalid credentials");
+        }
+
+        return user;
+      },
+    }),
+  ],
+});
